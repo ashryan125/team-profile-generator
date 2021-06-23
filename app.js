a5c41908d24d73f508b1cc3443c4e30def8e502c
ashryan <20115553+ashryan125@users.noreply.github.com>
2021-06-22 20:16:29 -0400

    html generated

--- app.js
+++ app.js
@@ -9,27 +9,212 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { createInflate } = require("zlib");
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+const employees = [];
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+const team = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the manager's name?"
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the manager's ID number? (numbers only)",
+            validate: (answer) => {
+                if (isNaN(answer)) {
+                    return "Please use numbers only";
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the manager's email address?"
+        },
+        {
+            type: 'number',
+            name: 'officeNumber',
+            message: "What is the manager's office number? (numbers only)",
+            validate: (answer) => {
+                if (isNaN(answer)) {
+                    return "Please use numbers only";
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'moreTeamMembers',
+            message: "Do you want to add a team member (Engineer or Intern)?",
+            default: true
+        },
+        {
+            type: 'list',
+            name: 'employeeType',
+            message: "What type of employee would you like to add??",
+            choices: ["Engineer", "Intern"],
+            when: ({ moreTeamMembers }) => {
+                if (moreTeamMembers) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+    ])
+        .then(function (data) {
+            let manager = new Manager(data.name, data.id, data.email, data.officeNumber);
+            employees.push(manager);
+            if (data.employeeType === 'Engineer') {
+                engineerQuestion();
+            } else if (data.employeeType === 'Intern') {
+                internQuestion();
+            } else {
+                createFile();
+            }
+        })
+}
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+const engineerQuestion = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the engineer's name?"
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is engineer's ID number? (numbers only)",
+            validate: (answer) => {
+                if (isNaN(answer)) {
+                    return "Please use numbers only";
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is engineer's email address?"
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "What is the engineer's github username?"
+        },
+        {
+            type: 'confirm',
+            name: 'moreTeamMembers',
+            message: "Do you want to add a team member (Engineer or Intern)?",
+            default: true
+        },
+        {
+            type: 'list',
+            name: 'employeeType',
+            message: "What type of employee would you like to add??",
+            choices: ["Engineer", "Intern"],
+            when: ({ moreTeamMembers }) => {
+                if (moreTeamMembers) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+    ])
+        .then(function (data) {
+            let engineer = new Engineer(data.name, data.id, data.email, data.github);
+            employees.push(engineer);
+            if (data.employeeType === 'Engineer') {
+                engineerQuestion();
+            } else if (data.employeeType === 'Intern') {
+                internQuestion();
+            } else {
+                createFile();
+            }
+        });
+}
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+const internQuestion = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the intern's name?"
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is intern's ID number? (numbers only)",
+            validate: (answer) => {
+                if (isNaN(answer)) {
+                    return "Please use numbers only";
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is intern's email address?"
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "What is the intern's school name?"
+        },
+        {
+            type: 'confirm',
+            name: 'moreTeamMembers',
+            message: "Do you want to add a team member (Engineer or Intern)?",
+            default: true
+        },
+        {
+            type: 'list',
+            name: 'employeeType',
+            message: "What type of employee would you like to add??",
+            choices: ["Engineer", "Intern"],
+            when: ({ moreTeamMembers }) => {
+                if (moreTeamMembers) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+    ])
+        .then(function (data) {
+            let intern = new Intern(data.name, data.id, data.email, data.school);
+            employees.push(intern);
+            if (data.employeeType === 'Engineer') {
+                engineerQuestion();
+            } else if (data.employeeType === 'Intern') {
+                internQuestion();
+            } else {
+                createFile();
+            }
+        });
+}
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+const createFile = () => {
+    let final = render(employees);
+    // if output directory does not exist, create it, then write file team.html
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR);
+    }
+    fs.writeFileSync(outputPath, final, "utf-8");
+    console.log('HTML file created in output folder. Open in browser!');
+};
+
+team();
